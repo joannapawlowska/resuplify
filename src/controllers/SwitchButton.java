a8b4c69e5a6d94a5801526af1ba2e2c9b1b330c8
Joanna Pawlowska <pawlo.joan@gmail.com>
2021-02-20 20:14:20 +0100

    Clean code

--- src/controllers/SwitchButton.java
+++ src/controllers/SwitchButton.java
@@ -10,27 +10,21 @@ import javafx.scene.shape.Circle;
 
 public class SwitchButton extends StackPane {
 
-    private Circle slider = new Circle(11);
-
-    private Label label = new Label();
-
-    private BooleanProperty switchedOn = new SimpleBooleanProperty(true);
-
+    private final Circle slider = new Circle();
+    private final Label label = new Label();
+    private final BooleanProperty switchedOn = new SimpleBooleanProperty(true);
     private static final PseudoClass SWITCHED_ON = PseudoClass.getPseudoClass("switched-on");
-
     private static final PseudoClass SWITCHED_OFF = PseudoClass.getPseudoClass("switched-off");
 
     public SwitchButton() {
-        init();
+        setSize();
+        setStylesheetAndStyleClasses();
+        setCurrentButtonStyle();
         addAction();
-    }
-
-    private void init() {
-        setStyle();
         getChildren().addAll(label, slider);
     }
 
-    private void setStyle() {
+    private void setSize(){
         setMinSize(60, 30);
         setPrefSize(60, 30);
         setMaxSize(60, 30);
@@ -39,28 +33,37 @@ public class SwitchButton extends StackPane {
         label.setPrefSize(45, 30);
         label.setMaxSize(45, 30);
 
+        slider.setRadius(11);
+    }
+
+    private void setStylesheetAndStyleClasses() {
         getStylesheets().add("css/switchButton.css");
         getStyleClass().add("switch-btn");
         slider.getStyleClass().add("switch-slider");
-
-        setButtonState();
     }
 
-    private void setButtonState(){
+    private void setCurrentButtonStyle(){
         if (switchedOn.get()) {
-            pseudoClassStateChanged(SWITCHED_ON, true);
-            pseudoClassStateChanged(SWITCHED_OFF, false);
-            label.setText("Yes");
+            setSwitchedOnStyle();
         } else {
-            pseudoClassStateChanged(SWITCHED_ON, false);
-            pseudoClassStateChanged(SWITCHED_OFF, true);
-            label.setText("No");
+            setSwitchedOffStyle();
         }
     }
 
-    private void addAction() {
+    private void setSwitchedOnStyle(){
+        pseudoClassStateChanged(SWITCHED_ON, true);
+        pseudoClassStateChanged(SWITCHED_OFF, false);
+        label.setText("Yes");
+    }
 
-        switchedOn.addListener(e -> setButtonState());
+    private void setSwitchedOffStyle(){
+        pseudoClassStateChanged(SWITCHED_ON, false);
+        pseudoClassStateChanged(SWITCHED_OFF, true);
+        label.setText("No");
+    }
+
+    private void addAction() {
+        switchedOn.addListener(e -> setCurrentButtonStyle());
     }
 
     public boolean isSwitchedOn() {
