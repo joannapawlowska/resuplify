757faa67faf9b81db4121da82fdf83212c491868
Joanna Pawlowska <pawlo.joan@gmail.com>
2021-03-06 23:27:14 +0100

    Extract non-negative text field

---
+++ src/components/NonNegativeIntegerTextField.java
@@ -0,0 +1,27 @@
+package components;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class NonNegativeIntegerTextField extends TextField {
+
+    public NonNegativeIntegerTextField(){
+        allowToPassOnlyNonNegativeIntegers();
+    }
+
+    private void allowToPassOnlyNonNegativeIntegers() {
+        textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!isNonNegativeInteger(newValue)) {
+                    setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+    }
+
+    private boolean isNonNegativeInteger(String text){
+        return text.matches("\\d+");
+    }
+}
