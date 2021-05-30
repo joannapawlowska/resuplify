7335bb333921aef76deeb5d50b4abe97e6ff3766
Joanna Pawlowska <pawlo.joan@gmail.com>
2021-05-30 23:57:05 +0200

    Update main scene, add action for log out pane

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/MainScene.fxml"));
-        stage.setTitle("Resuplify");
-        stage.setScene(new Scene(root));
-        stage.setMaximized(true);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        setUpStage(stage);
+    }
+
+    private void setUpStage(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/LoginScene.fxml"));
+        stage.setTitle("Resuplify");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
--- src/controllers/LogOutPaneController.java
+++ src/controllers/LogOutPaneController.java
@@ -1,11 +1,41 @@
 package controllers;
 
-import javafx.event.ActionEvent;
+import components.logic.Preference;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogOutPaneController {
 
+    @FXML AnchorPane logoutPane;
+
     @FXML
-    public void handleYesBtn(ActionEvent actionEvent) {
+    public void handleYesBtn() {
+        try {
+            Preference.clearToken();
+            setUpLoginScene();
+            closeCurrentStage();
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void setUpLoginScene() throws IOException{
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/LoginScene.fxml"));
+        stage.setTitle("Resuplify");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    private void closeCurrentStage(){
+        Stage currentStage = (Stage) logoutPane.getScene().getWindow();
+        currentStage.close();
     }
 }
